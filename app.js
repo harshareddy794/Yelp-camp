e69c4387ff18e5445257f0834b1e02fd4d3c5fc7
Gaddam Harshavardhan <harshareddy794@gmail.com>
2020-05-01 22:09:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+var express=require("express")
+var app=express()
+app.set("view engine","ejs")
+app.use(express.static("public"))
+var bodyparser=require("body-parser")
+app.use(bodyparser.urlencoded({extended:true}))
+var methodOverride= require("method-override")
+app.use(methodOverride("method"))
+var flash=require("connect-flash")
+app.use(flash())
+app.locals.moment = require('moment');
+require('dotenv').config()
+
+//+++++++++++++ mongoose connection ++++++++++++
+var mongoos=require("mongoose")
+mongoos.connect("mongodb://127.0.0.1:27017/Yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false})
+
+//++++++++++++ Passport initilize ++++++++++++++++++++
+var passport=require("passport")
+var localStratagy=require("passport-local")
+var expressSessions=require("express-session")
+
+app.use(expressSessions({
+    secret:process.env.SECRET,
+    resave: false,
+    saveUninitialized :false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+//+++++++++++++ Models +++++++++++++++++++++++++++
+var user=require("./models/user")
+
+
+//++++++++++++ Passport use ++++++++++++++++++++
+passport.use(new localStratagy(user.authenticate()))
+passport.serializeUser(user.serializeUser())
+passport.deserializeUser(user.deserializeUser())
+
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error=req.flash("error")
+    res.locals.success= req.flash("success")
+    next();
+ });
+
+
+ //+++++++++++++++ Routes +++++++++++++++++++++
+var userRoutes=require("./routes/index")
+var campRoutes=require("./routes/campground")
+var commentRoutes=require("./routes/comments")
+
+app.use(userRoutes)
+app.use(campRoutes)
+app.use(commentRoutes)
+
+//++++++++++++ Listening port +++++++++++++++++++
+app.listen(3000,"127.0.0.1",function(){
+    console.log("app is listining")
+})
